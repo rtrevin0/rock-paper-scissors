c22c0701c0bd21473f0add00de2dde256d349e5a
rtrevin0 <rickeytrevino@gmail.com>
2026-01-20 04:59:52 -0600

    add play round functionality

--- app.js
+++ app.js
@@ -17,5 +17,34 @@ function getHumanChoice() {
     return choice.toLowerCase();
 }
 
+function playRound(humanChoice, computerChoice) {
+    const humanChoiceLower = humanChoice.toLowerCase();
+    const computerChoiceLower = computerChoice.toLowerCase();
+
+    if (humanChoiceLower === computerChoiceLower) {
+        return "It's a tie!";
+    } else if (humanChoiceLower === "rock" && computerChoiceLower === "scissors"){
+        humanScore++;
+        return "You win! Rock beats scissors!";
+    } else if (humanChoiceLower === "paper" && computerChoiceLower === "rock"){
+        humanScore++;
+        return "You win! Paper beats rock!";
+    } else if (humanChoiceLower === "scissors" && computerChoiceLower === "paper"){
+        humanScore++;
+        return "You win! Scissors beats paper!";
+    } else if (humanChoiceLower === "rock" && computerChoiceLower === "paper") {
+        computerScore++;
+        return "You lose! Paper beats rock!";
+    } else if (humanChoiceLower === "paper" && computerChoiceLower === "scissors"){
+        computerScore++;
+        return "You lose! Scissors beats paper!";
+    } else if (humanChoiceLower === "scissors" && computerChoiceLower === "rock"){
+        computerScore++;
+        return "You lose! Rock beats scissors!";
+    } else {
+        return "Invalid choice! Please choose rock, paper, or scissors.";
+    }   
+}
+
 console.log(getComputerChoice());
 console.log(getHumanChoice());
